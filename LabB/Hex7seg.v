6e348fc58840ee39a9bce5882014d07e7e1cbd24
M Juma <github@mhjuma.com>
2014-05-29 11:28:48 -0700

    Finished Hex7Seg

--- LabB/Hex7seg.v
+++ LabB/Hex7seg.v
@@ -7,43 +7,53 @@ Antonio Orozco
 Lab B
 This module implements a 7-segment decoder (0 through 9)
 */
-module Hex7seg( C, Display );
-  input      [3:0] C;        // Input
-  output reg [0:6] Display;  // seven-segment display output
-  /*
-  *   0
-  *  ---
-  * |   |
-  *5|   |1
-  * | 6 |
-  *  ---
-  * |   |
-  *4|   |2
-  * |   |
-  *  ---
-  *   3
-  */
+module Hex7seg( c, display );
+
+    //-----------------------
+    // Input Ports
+    //-----------------------
+    input      [3:0] c;        // Input
+    
+    //-----------------------
+    // Output Ports
+    //-----------------------
+    output reg [0:6] display;  // Seven-segment display output
+    
+    /*
+    *   0
+    *  ---
+    * |   |
+    *5|   |1
+    * | 6 |
+    *  ---
+    * |   |
+    *4|   |2
+    * |   |
+    *  ---
+    *   3
+    */
   
   // Note: a '0' turns the segment ON
   // Note: Segment 6 is the left most bit
-  always @ ( C )
-    case ( C )		  //6......0 <----
-      1: Display = 7'b111_1001; //1
-      2: Display = 7'b010_0100; //2
-      3: Display = 7'b011_0000; //3
-      4: Display = 7'b001_1001; //4
-      5: Display = 7'b001_0010; //5
-      6: Display = 7'b000_0010; //6
-      7: Display = 7'b111_1000; //7
-      8: Display = 7'b000_0000; //8
-      9: Display = 7'b001_0000; //9
-		10: Display = 7'b000_1000; //A
-		11: Display = 7'b000_0011; //b
-		12: Display = 7'b100_0110; //C
-		13: Display = 7'b010_0001; //d
-		14: Display = 7'b000_0110; //E
-		15: Display = 7'b000_1110; //F
-      default: Display = 7'b100_0000; //0
-  endcase
-
+    always @ ( c )
+        begin
+            case ( c )		                    // 6......0 <----
+                1: display = 7'b111_1001;       // 1
+                2: display = 7'b010_0100;       // 2
+                3: display = 7'b011_0000;       // 3
+                4: display = 7'b001_1001;       // 4
+                5: display = 7'b001_0010;       // 5
+                6: display = 7'b000_0010;       // 6
+                7: display = 7'b111_1000;       // 7
+                8: display = 7'b000_0000;       // 8
+                9: display = 7'b001_0000;       // 9
+                10: display = 7'b000_1000;      // A
+                11: display = 7'b000_0011;      // b
+                12: display = 7'b100_0110;      // C
+                13: display = 7'b010_0001;      // d
+                14: display = 7'b000_0110;      // E
+                15: display = 7'b000_1110;      // F
+                default: display = 7'b100_0000; // 0
+            endcase
+        end
 endmodule
